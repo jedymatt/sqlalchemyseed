b40f2361ea74b9f35b1bca7185131c73730e6060
jedymatt <jedymatt@gmail.com>
2021-06-21 19:54:01 +0800

    Rewritten, added list support of 'data' field

--- sqlalchemyseed/__init__.py
+++ sqlalchemyseed/__init__.py
@@ -1,28 +1,23 @@
 import importlib
 import json
-import os
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
+
+
+class Label:
+    MODEL = 'model'
+    DATA = 'data'
+    FILTER = 'filter'
 
 
 def load_entities_from_json(json_filepath):
-    if not os.path.exists(json_filepath):
-        raise os.error.filename('File does not exist.')
-
-    with open(json_filepath, 'r') as f:
-        data = json.loads(f.read())
+    try:
+        with open(json_filepath, 'r') as f:
+            data = json.loads(f.read())
+    except FileNotFoundError as error:
+        raise FileNotFoundError(error)
 
     return data
-
-
-def _create_model_objects(session, data):
-    objects = []
-
-    for d in data:
-        res = _populate_data(session, d)
-        objects.append(res)
-        session.add(res)
-    return objects
 
 
 def _children_dict_or_list(data):
@@ -31,35 +26,76 @@ def _children_dict_or_list(data):
             yield key, value
 
 
-def _populate_data(session, data):
-    model = _get_model_class(data['model'])
+def get_model_instance(model: str):
+    try:
+        module_name, class_name = model.rsplit('.', 1)
+    except ValueError:
+        raise ValueError('Invalid module or class input format.')
 
-    if 'data' in data:
-        attr = data['data']
-
-        obj = model(**{k: v for k, v in attr.items() if not isinstance(v, dict) and not isinstance(v, list)})
-    elif 'filter' in data:
-        attr = data['filter']
-
-        obj = session.query(model).filter_by(**attr).one_or_none()
-    else:
-        raise KeyError('data or filter does not exist.')
-
-    for key, value in _children_dict_or_list(attr):
-        if isinstance(value, dict):
-            setattr(obj, key, _populate_data(session, value))
-        elif isinstance(value, list):
-            attrs = [_populate_data(session, i) for i in value]
-            setattr(obj, key, attrs)
-
-    return obj
-
-
-def _get_model_class(model):
-    module_name, class_name = str(model).rsplit('.', 1)
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
 
 
-def create_objects(session, data):
-    return _create_model_objects(session, data)
+def create_objects(entities, session=None, auto_add=False):
+    results = []
+
+    for entity in entities:
+        objects = _populate_entity(entity, session)
+
+        if auto_add:
+            session.add_all(objects)
+
+        for objects in objects:
+            results.append(objects)
+
+    return results
+
+
+def _populate_entity(entity, session=None):
+    if Label.MODEL not in entity:
+        raise KeyError("Field 'model' does not exist.")
+
+    model_instance = get_model_instance(entity[Label.MODEL])
+
+    if Label.DATA in entity:
+        label = Label.DATA
+    elif Label.FILTER in entity:
+        label = Label.FILTER
+        if session is None:
+            raise ValueError("session not found, it is required when using 'filter' field.")
+    else:
+        raise KeyError("'data' or 'filter' fields does not exist.")
+
+    if not isinstance(entity[label], list):
+        group_fields = [entity[label]]
+    else:
+        group_fields = entity[label]
+
+    objects = []
+    for fields in group_fields:
+        obj = _populate_fields(fields, model_instance, label, session)
+
+        objects.append(
+            obj
+        )
+
+    return objects
+
+
+def _populate_fields(fields, model_instance, label, session=None):
+    if label == Label.DATA:
+        obj = model_instance(**{k: v for k, v in fields.items() if not isinstance(v, dict) and not isinstance(v, list)})
+    elif label == Label.FILTER:
+        obj = session.query(model_instance).filter_by(**fields).one_or_none()
+    else:
+        raise KeyError("'data' or 'filter' fields does not exist.")
+
+    for key, value in _children_dict_or_list(fields):
+        if isinstance(value, dict):
+            _populate_entity(value, session)
+        elif isinstance(value, list):
+            attrs = [_populate_entity(entity, session)[0] for entity in value]
+            print(key, attrs)
+            setattr(obj, key, attrs)
+
+    return obj
