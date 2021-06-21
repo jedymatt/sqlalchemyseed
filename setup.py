97622a157f5c39537305291f390916490d3e9b62
jedymatt <jedymatt@gmail.com>
2021-06-21 10:09:33 +0800

    Added long description

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ import re
 
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
 # VERSION = ''
 with open('sqlalchemyseed/__init__.py', 'r') as f:
     pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
@@ -11,6 +14,8 @@ setup(
     name='sqlalchemyseed',
     author='jedymatt',
     url='https://github.com/jedymatt/sqlalchemyseed',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     version=VERSION,
     license='MIT',
     packages=['sqlalchemyseed'],
