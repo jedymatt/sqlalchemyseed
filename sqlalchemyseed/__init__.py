25c297f6fd56773ba56af4a6d29e4b85625a86dc
jedymatt <jedymatt@gmail.com>
2021-06-24 22:47:46 +0800

    Update v0.1.0

--- sqlalchemyseed/__init__.py
+++ sqlalchemyseed/__init__.py
@@ -1,7 +1,13 @@
 import importlib
 import json
+from inspect import isclass
 
-__version__ = '0.0.2'
+import pkg_resources
+from jsonschema import validate
+
+__version__ = '0.1.0'
+
+_SCHEMA_PATH = 'schema.json'
 
 
 class Label:
@@ -10,92 +16,152 @@ class Label:
     FILTER = 'filter'
 
 
+def validate_entities(entities):
+    schema_res = pkg_resources.resource_string('sqlalchemyseed', _SCHEMA_PATH)
+    schema = json.loads(schema_res)
+
+    validate(entities, schema)
+
+
 def load_entities_from_json(json_filepath):
     try:
         with open(json_filepath, 'r') as f:
-            data = json.loads(f.read())
+            entities = json.loads(f.read())
     except FileNotFoundError as error:
         raise FileNotFoundError(error)
 
-    return data
+    validate_entities(entities)
+    return entities
 
 
-def _children_dict_or_list(data):
-    for key, value in data.items():
-        if isinstance(value, dict) or isinstance(value, list):
-            yield key, value
+class _ClassRegistry:
+    def __init__(self):
+        self._classes = {}
+        self._modules = {}
+
+    def register_class(self, class_path):
+        try:
+            module_name, class_name = class_path.rsplit('.', 1)
+        except ValueError:
+            raise ValueError('Invalid module or class input format.')
+
+        if module_name not in self._modules:
+            self._modules[module_name] = importlib.import_module(module_name)
+
+        if class_name not in self._classes:
+            class_ = getattr(self._modules[module_name], class_name)
+
+            if isclass(class_):
+                self._classes[class_name] = class_
+            else:
+                raise TypeError("'{}' is not a class".format(class_name))
+
+    def get_class(self, class_path):
+        try:
+            class_name = class_path.rsplit('.', 1)[1]
+        except IndexError:
+            class_name = class_path
+
+        return self._classes[class_name]
 
 
-def get_model_instance(model: str):
-    try:
-        module_name, class_name = model.rsplit('.', 1)
-    except ValueError:
-        raise ValueError('Invalid module or class input format.')
+class Seeder:
+    def __init__(self):
+        self.class_registry = _ClassRegistry()
+        self.object_instances = []
 
-    module = importlib.import_module(module_name)
-    return getattr(module, class_name)
+    def seed(self, entities, session=None):
+        if isinstance(entities, dict):
+            self._entity(entities)
+        elif isinstance(entities, list):
+            self._entities(entities)
+
+        if session:
+            session.add_all(self.object_instances)
+
+    def _entity(self, entity, parent=None):
+        class_path = entity['model']
+        data, data_key = self._get_data(entity)
+
+        self.class_registry.register_class(class_path)
+
+        instance = None
+
+        if isinstance(data, dict):
+            instance = self._field(data, class_path, parent, data_key)
+        elif isinstance(data, list):
+            self._fields(data, class_path, parent, data_key)
+
+        return instance
+
+    @staticmethod
+    def _get_data(entity):
+        key = 'data'
+        return entity[key], key
+
+    def _entities(self, entities):
+        for entity in entities:
+            self._entity(entity)
+
+    def _create_instance(self, field, class_path, data_key):
+        kwargs_ = {k: v for k, v in field.items() if not isinstance(v, dict) and not isinstance(v, list)}
+        class_ = self.class_registry.get_class(class_path)
+
+        return class_(**kwargs_)
+
+    def _field(self, field: dict, class_path: str, parent=None, data_key=None):
+
+        instance = self._create_instance(field, class_path, data_key)
+
+        if parent is None:
+            self.object_instances.append(instance)
+            parent = instance
+
+        for parent_key, value in field.items():
+            if isinstance(value, dict):
+                child = self._entity(value, parent=parent)
+                setattr(parent, parent_key, child)
+            elif isinstance(value, list):
+                children = []
+                for i in value:
+                    children.append(
+                        self._entity(i, parent=parent)
+                    )
+                setattr(parent, parent_key, children)
+
+        return instance
+
+    def _fields(self, fields, model: str, parent=None, data_key=None):
+        for field in fields:
+            self._field(field, model, parent=parent, data_key=data_key)
 
 
-def create_objects(entities, session=None, auto_add=False):
-    results = []
-
-    for entity in entities:
-        objects = _populate_entity(entity, session)
-
-        if auto_add:
-            session.add_all(objects)
-
-        for objects in objects:
-            results.append(objects)
-
-    return results
-
-
-def _populate_entity(entity, session=None):
-    if Label.MODEL not in entity:
-        raise KeyError("Field 'model' does not exist.")
-
-    model_instance = get_model_instance(entity[Label.MODEL])
-
-    if Label.DATA in entity:
-        label = Label.DATA
-    elif Label.FILTER in entity:
-        label = Label.FILTER
+class HybridSeeder(Seeder):
+    def __init__(self, session, auto_add=True):
+        super().__init__()
         if session is None:
-            raise ValueError("session not found, it is required when using 'filter' field.")
-    else:
-        raise KeyError("'data' or 'filter' fields does not exist.")
+            raise ValueError('session not found.')
+        self.session = session
+        self.auto_add = auto_add
 
-    if not isinstance(entity[label], list):
-        group_fields = [entity[label]]
-    else:
-        group_fields = entity[label]
+    def seed(self, entities, session=None):
+        super().seed(entities, self.session)
 
-    objects = []
-    for fields in group_fields:
-        obj = _populate_fields(fields, model_instance, label, session)
+    def _get_data(self, entity):
+        try:
+            return super()._get_data(entity)
+        except KeyError:
+            key = 'filter'
+            return entity[key], key
 
-        objects.append(
-            obj
-        )
-
-    return objects
-
-
-def _populate_fields(fields, model_instance, label, session=None):
-    if label == Label.DATA:
-        obj = model_instance(**{k: v for k, v in fields.items() if not isinstance(v, dict) and not isinstance(v, list)})
-    elif label == Label.FILTER:
-        obj = session.query(model_instance).filter_by(**fields).one_or_none()
-    else:
-        raise KeyError("'data' or 'filter' fields does not exist.")
-
-    for key, value in _children_dict_or_list(fields):
-        if isinstance(value, dict):
-            _populate_entity(value, session)
-        elif isinstance(value, list):
-            attrs = [_populate_entity(entity, session)[0] for entity in value]
-            print(key, attrs)
-            setattr(obj, key, attrs)
-
-    return obj
+    def _create_instance(self, field, class_path, data_key):
+        if data_key == 'data':
+            instance = super()._create_instance(field, class_path, data_key)
+            if self.auto_add:
+                self.session.add(instance)
+            return instance
+        else:
+            class_ = self.class_registry.get_class(class_path)
+            return self.session.query(class_).filter_by(
+                **{k: v for k, v in field if not isinstance(v, dict) and not isinstance(v, list)}
+            ).one_or_none()
--- tests/main.py
+++ tests/main.py
@@ -1,29 +1,38 @@
-import json
-from types import SimpleNamespace
+import functools
+from timeit import default_timer as timer
 
-from jsonschema import Draft7Validator
-from jsonschema import ValidationError
+import db
+from sqlalchemyseed import HybridSeeder
+from sqlalchemyseed import load_entities_from_json
 
-SCHEMA_PATH = 'sqlalchemyseed/schema.json'
-DATA_PATH = 'tests/object_root.json'
-
-with open(SCHEMA_PATH, 'r') as f:
-    data = f.read()
-
-    schema = json.loads(data)
-    schema_instance = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-
-with open(DATA_PATH, 'r') as f:
-    entities = json.loads(f.read())
-
-Validator = Draft7Validator
-
-validator = Validator(schema)
-
-try:
-    validator.validate(entities)
-except ValidationError as error:
-    result = Draft7Validator.TYPE_CHECKER.is_type(error.instance, "object")
-    print(result)
+DATA_PATH = 'test.json'
 
 
+def benchmark(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = timer()
+        res = func(*args, **kwargs)
+        end = timer()
+        print(end - start)
+        return res
+
+    return wrapper
+
+
+db.create_tables()
+
+data = load_entities_from_json(DATA_PATH)
+
+seeder = HybridSeeder(db.session)
+
+
+@benchmark
+def test():
+    seeder.seed(data)
+
+
+test()
+print(seeder.object_instances)
+print(db.session.new)
+print(db.session.dirty)
