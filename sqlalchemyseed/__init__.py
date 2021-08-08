daa19cca14f26887a564487e29b5b729d6d5bd4d
jedymatt <jedymatt@gmail.com>
2021-08-08 08:58:30 +0800

    Bump version to v0.4.1 - update imports

--- sqlalchemyseed/__init__.py
+++ sqlalchemyseed/__init__.py
@@ -1,6 +1,6 @@
+from .seeder import ClassRegistry
 from .seeder import HybridSeeder
 from .seeder import Seeder
-from .seeder import ClassRegistry
+from .seeder import load_entities_from_json
 
-
-__version__ = '0.4.0'
+__version__ = '0.4.1'
