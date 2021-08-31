ab74bfde75fbc60fcece0b521e1681d87f888793
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-08-31 13:08:03 +0800

    Fix path directory of __init__.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 import re
-import logging
 
 from setuptools import setup
 
@@ -8,48 +7,9 @@ with open("README.md", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
 
-with open(os.path.join('src', '__init__.py'), 'r') as f:
+with open(os.path.join('src', 'sqlalchemyseed', '__init__.py'), 'r') as f:
     pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
     VERSION = re.search(pattern, f.read(), re.MULTILINE).group(1)
 
 
-# packages = ['src']
-
-# install_requires = [
-#     'SQLAlchemy>=1.4',
-# ]
-
-# extras_require = {
-#     'yaml': ['PyYAML>=5.4']
-# }
-
-
-setup(
-    # name='sqlalchemyseed',
-    version=VERSION,
-    # description='SQLAlchemy seeder.',
-    # long_description=LONG_DESCRIPTION,
-    # long_description_content_type='text/markdown',
-    # url='https://github.com/jedymatt/sqlalchemyseed',
-    # author='jedymatt',
-    # author_email='jedymatt@gmail.com',
-    # license='MIT',
-    # packages=packages,
-    # package_data={'sqlalchemyseed': ['res/*']},
-    # install_requires=install_requires,
-    # extras_require=extras_require,
-    # python_requires='>=3.6',
-    # project_urls={
-    #     'Source': 'https://github.com/jedymatt/sqlalchemyseed',
-    #     'Tracker': 'https://github.com/jedymatt/sqlalchemyseed/issues',
-    # },
-    # classifiers=[
-    #     'License :: OSI Approved :: MIT License',
-
-    #     'Programming Language :: Python :: 3.6',
-    #     'Programming Language :: Python :: 3.7',
-    #     'Programming Language :: Python :: 3.8',
-    #     'Programming Language :: Python :: 3.9',
-    # ],
-    # keywords='sqlalchemy, seed, seeder, json, yaml',
-)
+setup(version=VERSION,)
