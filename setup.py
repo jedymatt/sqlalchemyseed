e19fe17d1e5e86bebf09b5790258f7f3979d33b4
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-08-31 13:36:22 +0800

    Update version location

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ import re
 
 from setuptools import setup
 
-with open("README.md", encoding="utf-8") as f:
-    LONG_DESCRIPTION = f.read()
+
+# with open(os.path.join('src', 'sqlalchemyseed', '__init__.py'), 'r') as f:
+#     pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
+#     VERSION = re.search(pattern, f.read(), re.MULTILINE).group(1)
 
 
-with open(os.path.join('src', 'sqlalchemyseed', '__init__.py'), 'r') as f:
-    pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    VERSION = re.search(pattern, f.read(), re.MULTILINE).group(1)
-
-
-setup(version=VERSION,)
+# setup(version=VERSION,)
+setup()
--- tests/scratch.py
+++ tests/scratch.py
@@ -6,3 +6,7 @@ from models import Base
 engine = create_engine('sqlite://')
 Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
+
+import sqlalchemyseed
+
+print(sqlalchemyseed.__version__)
