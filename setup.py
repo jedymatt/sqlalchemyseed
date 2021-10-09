72c95cbaf9827c72057c4948b30fff277c2faa1a
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-10-09 18:40:06 +0800

    Removed unnecessary comments

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-import os
-import re
-
 from setuptools import setup
 
 
-# with open(os.path.join('src', 'sqlalchemyseed', '__init__.py'), 'r') as f:
-#     pattern = r"^__version__ = ['\"]([^'\"]*)['\"]"
-#     VERSION = re.search(pattern, f.read(), re.MULTILINE).group(1)
-
-
-# setup(version=VERSION,)
 setup()
