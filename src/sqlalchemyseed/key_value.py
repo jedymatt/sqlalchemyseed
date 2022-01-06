cad26649eaf2c0d72d89fe7427498ce7ea3a05b4
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-01-06 20:19:46 +0800

    Added attribute wrapper class

--- src/sqlalchemyseed/key_value.py
+++ src/sqlalchemyseed/key_value.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 
-import sqlalchemy
-from sqlalchemy import inspect, orm
-from sqlalchemy.orm.attributes import InstrumentedAttribute, QueryableAttribute
-from sqlalchemy.orm.state import AttributeState, InstanceState
+from sqlalchemy import ForeignKey, inspect, orm
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+from sqlalchemy.orm.properties import ColumnProperty
+from sqlalchemy.orm.relationships import RelationshipProperty
 
-from sqlalchemyseed.util import is_supported_class
+from sqlalchemyseed import errors
 
 
 def get_class(obj):
@@ -21,24 +21,171 @@ class KeyValue:
     Key-value pair class
     """
     key: str
-    value: object = None
+    value: object
 
 
-@dataclass
-class Attribute:
+# @dataclass
+# class AttributeWrapper:
+#     """
+#     AttributeWrapper class is an InstrumentedAttribute wrapper
+#     """
+
+#     attribute: InstrumentedAttribute
+#     """
+#     An InstrumentedAttribute
+#     """
+
+#     is_column: bool
+#     """
+#     True if the attribute is a column
+
+#     Ex.:
+#         class SomeClass:
+#             ...
+
+#             value = Column(...)
+#     """
+
+#     is_relationship: bool
+#     """
+#     True if the attribute is a relationship
+
+#      Ex.:
+#         class SomeClass:
+#             ...
+
+#             value = relationship(...)
+#     """
+
+#     @classmethod
+#     def from_attribute(cls, attr: object):
+#         """
+#         From attribute
+#         """
+#         insp: InstrumentedAttribute = inspect(attr, raiseerr=False)
+
+#         if insp is None or not insp.is_attribute:
+#             raise errors.InvalidTypeError("Invalid class attribute")
+
+#         attribute: InstrumentedAttribute = insp
+#         is_column = isinstance(insp.property, ColumnProperty)
+#         is_relationship = isinstance(insp.property, RelationshipProperty)
+
+#         return cls(attribute=attribute, is_column=is_column, is_relationship=is_relationship)
+
+#     @classmethod
+#     def from_instance(cls, instance: object, attribute_name: str):
+#         """
+#         From instance
+#         """
+#         attr = getattr(instance.__class__, attribute_name)
+#         return cls.from_attribute(attr)
+
+#     @property
+#     def parent(self) -> orm.Mapper:
+#         """
+#         Parent of the attribute which is a class mapper
+#         """
+#         return self.attribute.parent
+
+class AttributeWrapper:
     """
-    Attribute class
+    AttributeWrapper class. A wrapper for InstrumentAttribute.
     """
-    value: InstrumentedAttribute
+    attr: InstrumentedAttribute
+    """
+    An InstrumentAttribute.
+    To get an InstrumentedAttribute use `inspect(Class.attribute, raiseerr=False)`
+    """
+    is_column: bool
+    """
+    True if the attribute is a column
+
+    Ex.:
+        class SomeClass:
+            ...
+
+            value = Column(...)
+    """
+
+    is_relationship: bool
+    """
+    True if the attribute is a relationship
+
+     Ex.:
+        class SomeClass:
+            ...
+
+            value = relationship(...)
+    """
+
+    def __init__(self, class_attribute) -> None:
+        class_attribute = inspect(class_attribute, raiseerr=False)
+
+        if class_attribute is None or not class_attribute.is_attribute:
+            raise errors.InvalidTypeError("Invalid class attribute")
+
+        self.attr: InstrumentedAttribute = class_attribute
+        self.is_column = isinstance(class_attribute.property, ColumnProperty)
+        self.is_relationship = isinstance(
+            class_attribute.property,
+            RelationshipProperty,
+        )
+        self._cache_ref_class = None
 
     @property
-    def property(self):
+    def parent(self) -> orm.Mapper:
         """
-        Attribute property
+        Parent of the attribute which is a class mapper.
         """
-        return self.value.property
-        # return self.value.property
-# key_value = KeyValue(key="key")
 
-# print(key_value == KeyValue(key="key", value="fre"))
-# print(str(key_value))
+        return self.attr.parent
+
+    @property
+    def class_(self) -> object:
+        """
+        Returns a parent class.
+        """
+        return self.attr.parent.class_
+
+    @property
+    def prop(self):
+        """
+        MapperProperty
+        """
+        return self.attr.property
+
+    @property
+    def referenced_class(self):
+        """
+        Referenced class.
+        Returns None if there is no referenced class
+        """
+        if self._cache_ref_class is not None:
+            return self._cache_ref_class
+
+        if self.is_relationship:
+            self._cache_ref_class = self.attr.mapper.class_
+            return self._cache_ref_class
+
+        if self.is_column:
+            foreign_key: ForeignKey = next(iter(self.attr.foreign_keys), None)
+            if foreign_key is None:
+                return None
+
+            self._cache_ref_class = next(filter(
+                lambda mapper: mapper.tables[0].name == foreign_key.column.table.name,
+                self.attr.parent.registry.mappers
+            )).class_
+
+        return self._cache_ref_class
+
+
+class AttributeValue(AttributeWrapper):
+    """
+    Value container for AttributeWrapper
+    """
+
+    def __init__(self, attr, value) -> None:
+        super().__init__(attr)
+        self.value = value
--- tests/scratch.py
+++ tests/scratch.py
@@ -5,14 +5,60 @@ Scratch file
 import logging
 
 import sqlalchemy
+from sqlalchemy.orm.attributes import set_attribute
+from sqlalchemy.orm.base import class_mapper, object_mapper, object_state
+from sqlalchemy.orm.interfaces import MapperProperty
+from sqlalchemy.orm.relationships import foreign
 import sqlalchemy.orm.state
-from models import Base, Parent, Single
-from sqlalchemy import create_engine, inspect
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, create_engine, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import ColumnProperty, relationship, sessionmaker
+from sqlalchemy.sql.base import ColumnCollection, Immutable, ImmutableColumnCollection
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.util._collections import ImmutableProperties
 from sqlalchemy_utils import generic_repr
 
 import sqlalchemyseed
+from sqlalchemyseed.key_value import *
+from sqlalchemyseed.seeder import Entity, EntityTuple
 from sqlalchemyseed.util import generate_repr
+
+Base = declarative_base()
+
+
+class Single(Base):
+    """
+    A class with no child
+    """
+    __tablename__ = 'single'
+    id = Column(Integer, primary_key=True)
+    value = Column(String(45))
+
+    def __repr__(self) -> str:
+        return generate_repr(self)
+
+
+class Parent(Base):
+    __tablename__ = 'parents'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    children = relationship('Child')
+
+    def __repr__(self) -> str:
+        return generate_repr(self)
+
+
+class Child(Base):
+    __tablename__ = 'children'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    parent_id = Column(Integer, ForeignKey('parents.id'))
+
+    def __repr__(self) -> str:
+        return generate_repr(self)
+
 
 engine = create_engine('sqlite://')
 Session = sessionmaker(bind=engine)
@@ -20,14 +66,38 @@ Base.metadata.create_all(engine)
 
 print(sqlalchemyseed.__version__)
 
+single = Single(value='343')
 
-single = Single()
-mapper: sqlalchemy.orm.state.InstanceState = inspect(
-    Parent.children, raiseerr=False)
+# print(single)
+# print(generate_repr(single))
 
-print(single)
-print(generate_repr(single))
-# var = inspect(single, raiseerr=False)
-# print(vars(inspect(Single.id)))
-# print("=============================")
-# print(mapper.is_mapper)
+wrapped = AttributeValue(Child.name, 1)
+attr: InstrumentedAttribute = wrapped.attr
+parent: orm.Mapper = attr.parent
+comp: ColumnProperty.Comparator = attr.comparator
+prop: ColumnProperty = attr.property
+
+print(parent.tables[0].name)
+print(wrapped.referenced_class)
+
+
+# entity = Entity(instance=Parent(name="John Doe"), attr_name='name')
+
+# print(entity.referenced_class)
+
+
+for col in wrapped.parent.columns:
+    print(col)
+    foreign_keys = tuple(col.foreign_keys)
+    if len(foreign_keys) > 0:
+        foreign_keys = foreign_keys[0]
+        col_: Column = foreign_keys.column
+        print(col_.table.name)
+        class_ = next(filter(
+            lambda mapper: mapper.class_.__tablename__ == col_.table.name,
+            wrapped.parent.registry.mappers
+        )).class_
+        print(f"referenced class: {class_}")
+        # print(foreign_keys.column.key)
+        # print(foreign_keys.column.name)
+    print()
