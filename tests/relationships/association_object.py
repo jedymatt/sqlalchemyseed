e3694c371bb25ec471c257aea401fcfac7bab673
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-01-12 14:10:15 +0800

    Changed attribute name

--- tests/relationships/association_object.py
+++ tests/relationships/association_object.py
@@ -9,7 +9,7 @@ class Association(Base):
     __tablename__ = 'association'
     left_id = Column(ForeignKey('left.id'), primary_key=True)
     right_id = Column(ForeignKey('right.id'), primary_key=True)
-    extra_data = Column(String(45))
+    extra_value = Column(String(45))
     child = relationship("Child", back_populates="parents")
     parent = relationship("Parent", back_populates="children")
 
