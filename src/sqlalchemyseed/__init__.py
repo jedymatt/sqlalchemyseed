5be4f610ff46bcaff2b813499db81fbe4730e84d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-27 19:02:52 +0800

    Bump version to 1.0.6-dev

--- src/sqlalchemyseed/__init__.py
+++ src/sqlalchemyseed/__init__.py
@@ -29,7 +29,7 @@ from .loader import load_entities_from_yaml
 from .loader import load_entities_from_csv
 
 
-__version__ = "1.0.5"
+__version__ = "1.0.6-dev"
 
 if __name__ == '__main__':
     pass
