cead62894b5ff26bfe8cd761c8cb0843ffc4f233
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-31 10:52:24 +0800

    Removed HybridSeeder import

--- src/sqlalchemyseed/__init__.py
+++ src/sqlalchemyseed/__init__.py
@@ -2,7 +2,7 @@
 SQLAlchemy seeder that supports nested relationships with an easy to read text files.
 """
 
-from .seeder import HybridSeeder
+
 from .seeder import Seeder
 from .loader import load_entities_from_json
 from .loader import load_entities_from_yaml
