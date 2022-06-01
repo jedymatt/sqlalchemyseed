93ff7abac2b655eed64acc21b4d6c03515520c47
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-01 11:04:29 +0000

    Rewrite models

--- tests/models.py
+++ tests/models.py
@@ -7,173 +7,80 @@ Base = declarative_base()
 AnotherBase = declarative_base()
 
 
-class Company(Base):
-    __tablename__ = 'companies'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), unique=True)
-
-    employees = relationship('Employee', back_populates='company')
-
-    def __repr__(self) -> str:
-        return f"<Company(name='{self.name}')>"
-
-
-class Employee(Base):
-    __tablename__ = 'employees'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-
-    company_id = Column(Integer, ForeignKey('companies.id'))
-
-    company = relationship(
-        'Company', back_populates='employees', uselist=False)
-
-    def __repr__(self) -> str:
-        return f"<Employee(name='{self.name}', company='{self.company}')>"
-
-
-class Parent(Base):
-    __tablename__ = 'parents'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    children = relationship('Child')
-
-    def __repr__(self) -> str:
-        return f"<Parent(name='{self.name}', children='{[child.name if child.name is not None else child for child in self.children]}')>"
-
-
-class Child(Base):
-    __tablename__ = 'children'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    parent_id = Column(Integer, ForeignKey('parents.id'))
-
-    children = relationship('GrandChild')
-
-
-class GrandChild(Base):
-    __tablename__ = 'grand_children'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    parent_id = Column(Integer, ForeignKey('children.id'))
-
-
 class Person(Base):
+    """
+    Person class
+    """
     __tablename__ = 'persons'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    age = Column(Integer)
+
+    location_id = Column(Integer, ForeignKey('locations.id'))
+
+    spouse = relationship('Spouse', uselist=False, back_populates='person')
+    location = relationship('Location', back_populates='persons')
+    cars = relationship('Car', back_populates='owner')
+
+    def __repr__(self):
+        return generate_repr(self)
+
+
+class Car(Base):
+    """
+    Car class.
+    """
+    __tablename__ = 'cars'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    owner_id = Column(Integer, ForeignKey('persons.id'))
+
+    owner = relationship('Person', back_populates='cars')
+
+    def __repr__(self) -> str:
+        return generate_repr(self)
+
+
+class Spouse(Base):
+    """
+    Spouse class.
+    """
+    __tablename__ = 'spouses'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    age = Column(Integer)
+    person_id = Column(Integer, ForeignKey('persons.id'))
+
+    person = relationship('Person', back_populates='spouse')
+
+    def __repr__(self) -> str:
+        return generate_repr(self)
+
+
+class Location(Base):
+    """
+    Location class.
+    """
+    __tablename__ = 'locations'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
 
+    persons = relationship('Person', back_populates='location')
 
-not_class = 'this is not a class'
-
-
-class UnsupportedClass:
-    """This is an example of an unsupported class"""
-    pass
+    def __repr__(self) -> str:
+        return generate_repr(self)
 
 
-class AnotherCompany(AnotherBase):
+class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(255), unique=True)
-
-    employees = relationship('Employee', back_populates='company')
-
-    def __repr__(self) -> str:
-        return f"<AnotherEmployee(name='{self.name}')>"
-
-
-class AnotherEmployee(AnotherBase):
-    __tablename__ = 'employees'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-
-    company_id = Column(Integer, ForeignKey('companies.id'))
-
-    company = relationship(
-        'Company', back_populates='employees', uselist=False)
-
-    def __repr__(self) -> str:
-        return f"<AnotherEmployee(name='{self.name}', company='{self.company}')>"
-
-
-class Single(Base):
-    """
-    Single class with no child
-    """
-    __tablename__ = 'single'
-    id = Column(Integer, primary_key=True)
-    value = Column(String(45))
+    name = Column(String(50))
+    address = Column(String(50))
 
     def __repr__(self) -> str:
         return generate_repr(self)
-
-
-class One(Base):
-    """
-    One class with no other Parent that relates to its child.
-    """
-    __tablename__ = 'single_parent'
-
-    id = Column(Integer, primary_key=True)
-    value = Column(String(45))
-
-    def __repr__(self) -> str:
-        return generate_repr(self)
-
-
-class OneToOne(Base):
-    """
-    OneToOne class
-    """
-    __tablename__ = 'one_to_one'
-
-    id = Column(Integer, primary_key=True)
-    value = Column(String(45))
-
-    def __repr__(self) -> str:
-        return generate_repr(self)
-
-class OneToMany(Base):
-    """
-    OneToMany class
-    """
-    __tablename__ = 'one_to_many'
-
-    id = Column(Integer, primary_key=True)
-
-    def __repr__(self) -> str:
-        return generate_repr(self)
-
-
-class ManyToOne(Base):
-    """
-    ManyToOne class
-    """
-    __tablename__ = 'many_to_one'
-
-    id = Column(Integer, primary_key=True)
-
-
-class ManyToMany(Base):
-    """
-    ManyToMany class
-    """
-    __tablename__ = 'many_to_many'
-
-    id = Column(Integer, primary_key=True)
-
-    def __repr__(self) -> str:
-        return generate_repr(self,)
-
-
-val = Single(value='str value')
-print(repr(val))
